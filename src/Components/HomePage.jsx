a6fffa8965a6d0e68818f0b022ce966ed2ccc203
Prakhar <prakhar24680@gmail.com>
2020-12-05 13:41:00 +0530

    q'

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -4,11 +4,12 @@ import SentMessages from './sentMessages';
 
 const HomePage = () => {
 
-    return(
-    <div style={{display: 'grid', gridTemplateColumns : "400px auto", gridGap : "40px"}}>
-        <AddUsers/>
-        <SentMessages/>
-    </div>
+    return (
+        <div style={{ display: 'grid', gridTemplateColumns: "400px auto", gridGap: "40px" }}>
+            <p>Hii, please use the json files sent in the folder named dummy Json files to test. Use contact.json for list of contacts and sentMessages.json for list of sent messages</p>
+            <AddUsers />
+            <SentMessages />
+        </div>
     )
 
 }
